3dd532f45c009bcbc8478b526d9d8ccb8649f0ea
Eric Su <ericsu@wireless-10-146-98-104.public.utexas.edu>
2018-11-02 19:45:22 -0500

    added on campus feature

--- app/src/main/java/com/example/android/mapapp/SearchActivity.java
+++ app/src/main/java/com/example/android/mapapp/SearchActivity.java
@@ -7,6 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+
 /**
  * Created by gydoy on 11/1/2018.
  */
@@ -16,9 +20,31 @@ public class SearchActivity extends AppCompatActivity {
     public void sendLocation(View view){
         Intent intent = new Intent(this, MapsActivity.class);
         EditText editText =(EditText) findViewById(R.id.location_query);
-        String message = editText.getText().toString();
+        StringBuilder message = editText.getText().toString();
+        boolean oncampus=true;
+
+
+		Geocoder geocoder = new Geocoder(this);
+        try {
+            addressList = geocoder.getFromLocationName(message,1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Address res = addressList.get(0);
+        if (res.getLatitude()>30.292 && res.getLatitude()<30.281){
+        	if (res.getLongitude()> (-97.728) && res.getLongitude()< (-97.742){
+        		oncampus=false;
+        	}
+        }
+
+
+
+
+        if (oncampus){message.append(" you're on campus");}
+        
         String dummyAddress = "1600 Amphitheatre Parkway, Mountain View, CA";
-        intent.putExtra("location", message);
+        intent.putExtra("location", message.toString);
         startActivity(intent);
     }
 
